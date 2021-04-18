f58a94fdbb7c0d5ea7f5636f5186c94ef679a073
shioko <ichisadashioko@gmail.com>
2021-04-18 02:24:23 +0000

    read UTF-8 text file within /data/local/tmp

--- app/src/main/java/io/github/ichisadashioko/android/adbcopypaste/Copy.java
+++ app/src/main/java/io/github/ichisadashioko/android/adbcopypaste/Copy.java
@@ -1,9 +1,59 @@
 // package io.github.ichsadashioko.android.adbcopypaste;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Copy {
-    public static void main(String[] args){
-        for(int i = 0; i < args.length; i++){
-            System.out.println(i + " - " + args[i]);
+    public static void main(String[] args) {
+        int returncode = 0;
+
+        try {
+            // for(int i = 0; i < args.length; i++){
+            //     System.out.println(i + " - " + args[i]);
+            // }
+
+            if (args.length < 1) {
+                System.err.println("file path of data to be copied was not supplied");
+                returncode = 1;
+            } else {
+                String text_data_filepath = args[0];
+                File text_data_file = new File(text_data_filepath);
+                if (!text_data_file.exists()) {
+                    System.err.println(text_data_filepath + " does not exist");
+                    returncode = 1;
+                } else {
+                    if (!text_data_file.isFile()) {
+                        System.err.println(text_data_filepath + " is not a normal file");
+                        returncode = 1;
+                    } else {
+                        Path path = Paths.get(text_data_filepath);
+                        int text_data_file_size = (int) Files.size(path);
+                        if (text_data_file_size < 1) {
+                            System.err.println(text_data_filepath + " is empty");
+                            returncode = 1;
+                        } else if (text_data_file_size > (5 * 1024 * 1024)) {
+                            throw new Exception(text_data_file_size + " is unreasonably large");
+                        } else {
+                            byte[] text_file_bytes = new byte[text_data_file_size];
+                            FileInputStream file_input_stream = new FileInputStream(text_data_file);
+                            file_input_stream.read(text_file_bytes);
+                            file_input_stream.close();
+
+                            String text_content =
+                                    new String(text_file_bytes, StandardCharsets.UTF_8);
+                            System.out.println(text_content);
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(System.err);
+            returncode = -1;
         }
+
+        System.exit(returncode);
     }
 }
--- compile_dex_executable_note.sh
+++ compile_dex_executable_note.sh
@@ -4,8 +4,11 @@ javac -source 1.7 -target 1.7 HelloWorld.java
 
 $ANDROID_SDK_ROOT/build-tools/30.0.3/dx --dex --output io.github.ichisadashioko.android.adbcopypaste.Copy.dex Copy.class
 
-$ANDROID_SDK_ROOT/build-tools/30.0.3/dx --dex --output Copy.dex Copy.class
 
 adb shell CLASSPATH=/data/local/tmp/classes.dex app_process / HelloWorld
 
-adb shell CLASSPATH=/data/local/tmp/Copy.dex app_process / Copy
+javac -source 1.8 -target 1.8 Copy.java
+$ANDROID_SDK_ROOT/build-tools/30.0.3/dx --dex --output Copy.dex Copy.class
+adb shell rm /data/local/tmp/Copy.dex
+adb push Copy.dex /data/local/tmp/
+adb shell CLASSPATH=/data/local/tmp/Copy.dex app_process / Copy /data/local/tmp/utf8.txt
